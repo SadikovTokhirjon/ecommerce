7096ea9d92944a2233515671da4a53f8badb7366
SadikovTokhirjon <116348575+SadikovTokhirjon@users.noreply.github.com>
2024-11-11 22:00:03 +0500

    Template o'zgartirishlar kiritik

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category, Product,Order,Customer
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+
+
