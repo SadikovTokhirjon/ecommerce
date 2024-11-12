f08328157ad7acc98dd7c9e839034fd57be4cea5
SadikovTokhirjon <116348575+SadikovTokhirjon@users.noreply.github.com>
2024-11-13 00:12:22 +0500

    Login va log out qo'shdik

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from store.models import Product
+from django.contrib.auth import authenticate , login,logout
+
 # Create your views here.
 def home(request):
     products=Product.objects.all()
@@ -7,3 +9,23 @@ def home(request):
 
 def about(request):
     return render(request , 'aboute.html',{})
+
+
+def login_user(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else: return redirect('home')
+    else:return render(request,'login.html',{})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+
+
