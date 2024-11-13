3725753040310edc447aff2d13350093b8a8261b
SadikovTokhirjon <116348575+SadikovTokhirjon@users.noreply.github.com>
2024-11-13 11:53:10 +0500

    messages, register qismlarni qo'shdik

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render ,redirect
 from store.models import Product
 from django.contrib.auth import authenticate , login,logout
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     products=Product.objects.all()
@@ -26,6 +29,23 @@ def logout_user(request):
     logout(request)
     return redirect('home')
 
-
+def register_user(request):
+    form=SignUpForm()
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username= form.cleaned_data
+            password= form.cleaned_data
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,'You are now registered')
+            return redirect('home')
+        else:
+            messages.error(request,'Please correct the error below.')
+            return redirect('register')
+    else:
+        messages.error(request,'Please correct the error below.')
+        return render(request,'register.html',{'form':form})
 
 
